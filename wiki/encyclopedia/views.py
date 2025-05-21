fb973c1388ef5be2c3cdc840837f20a118650e1b
Matias  Ricart <matiasricart@Mac.local>
2025-05-21 23:17:35 +0200

    Add Entry and Index Page

---
+++ wiki/encyclopedia/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    content = util.get_entry(title)
+    if content is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "The requested entry was not found."
+        })
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": content
+    })
